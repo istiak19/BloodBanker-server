2dba52b063d1cfa2216555ce48c8db6eeea6ce89
istiak19 <istiak15-14128@diu.edu.bd>
2025-01-18 18:07:00 +0600

    volunteer api

--- index.js
+++ index.js
@@ -80,16 +80,16 @@ async function run() {
             }
             next()
         }
-        // const verifyVolunteer = async (req, res, next) => {
-        //     const email = req?.decoded?.email;
-        //     const query = { email: email };
-        //     const user = await userCollection.findOne(query);
-        //     const isVolunteer = user?.role === 'volunteer';
-        //     if (!isVolunteer) {
-        //         return res.status(403).send({ message: 'forbidden access' })
-        //     }
-        //     next()
-        // }
+        const verifyVolunteer = async (req, res, next) => {
+            const email = req?.decoded?.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isVolunteer = user?.role === 'volunteer';
+            if (!isVolunteer) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            next()
+        }
 
         // user api
         app.get('/user', verifyToken, verifyAdmin, async (req, res) => {
@@ -134,19 +134,19 @@ async function run() {
             res.send({ admin })
         })
 
-        // app.get('/users/volunteer/:email', verifyToken, verifyVolunteer, async (req, res) => {
-        //     const email = req.params.email;
-        //     if (email !== req?.decoded?.email) {
-        //         return res.status(403).send({ message: 'Unauthorized access' });
-        //     }
-        //     const query = { email: email };
-        //     const user = await userCollection.findOne(query);
-        //     let volunteer = false;
-        //     if (user) {
-        //         volunteer = user?.role === 'volunteer'
-        //     }
-        //     res.send({ volunteer })
-        // })
+        app.get('/users/volunteer/:email', verifyToken, verifyVolunteer, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req?.decoded?.email) {
+                return res.status(403).send({ message: 'Unauthorized access' });
+            }
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let volunteer = false;
+            if (user) {
+                volunteer = user?.role === 'volunteer'
+            }
+            res.send({ volunteer })
+        })
 
         app.post('/user', async (req, res) => {
             const user = req.body;
